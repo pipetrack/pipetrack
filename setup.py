28afed00b556c64f8862dc6c5a271f79930bcd37
Andrew Atamanyuk <a.atamantyk@corp.mail.ru>
2020-03-16 03:24:41 +0300

    import module directly

--- setup.py
+++ setup.py
@@ -1,18 +1,17 @@
-from setuptools import setup, find_namespace_packages
+from setuptools import setup, find_packages
 
 with open("README.md", "r") as readme_file:
     readme = readme_file.read()
 
 setup(
     name="pipetrack",
-    version="0.0.8",
+    version="0.0.9",
     author="Atamanyuk Andrew & Gajiev Kirill",
     description="A package to track steps of your ML projects",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://github.com/pipetrack/pipetrack",
-    package_dir={"": "pipetrack"},
-    packages=find_namespace_packages(where="pipetrack"),
+    packages=find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3.7",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
